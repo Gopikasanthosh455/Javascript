af69356c292b8bf36783fd70ea95067656e63486
Gopika santhosh <165985827+Gopikasanthosh455@users.noreply.github.com>
2025-02-05 10:56:23 +0530

    Add files via upload

---
+++ JavaScript/Datatypes/introduction.js
@@ -0,0 +1,24 @@
+// numbers
+let x =20;
+let y =30;
+
+//strings
+let name="Rahul";
+let course="python";
+
+//Boolean
+let a =true;
+let b =false;
+
+//Objects
+const Student ={
+    name : "Gopika",
+    age : 21,
+    place:"Guruvayoor"
+};
+
+// Array Objects
+const Skills=["python","css","HTML","Javascript","Django"]
+
+//date objects
+const date =new Date("2025-01-18");
---
+++ JavaScript/conditional_statements/demo.js
@@ -0,0 +1,4 @@
+x=10;
+y=19;
+sum=x+y;
+console.log(sum)
